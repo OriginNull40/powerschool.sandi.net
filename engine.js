c8325f6c6935b6dbcd0b70ec79d315efdb9ad000
OriginNull40 <originnull26@gmail.com>
2026-02-05 09:57:32 -0800

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,1 +1,3 @@
-import('https://cdn.jsdelivr.net/npm/@webrcade/app-common@1.2.10/dist/index.min.js');
+var script = document.createElement('script');
+script.src = 'https://cdn.jsdelivr.net/npm/@webrcade/app-common@1.2.10/dist/index.min.js';
+document.head.appendChild(script);
